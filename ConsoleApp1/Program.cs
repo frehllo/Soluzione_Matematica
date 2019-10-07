e489ee3d15a17cc437428f4da46ad281438ec488
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-07 09:56:14 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Operazioni;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il primo numero");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il secondo numero");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il terzo numero");
+            int c = int.Parse(Console.ReadLine());
+
+            bool r = Operazioni.Uguale(a, b, c);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+                              
+    }
+}
---
+++ Operazioni/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Operazioni
+{
+    public class Metodi
+    {
+        public static bool Uguale(int a, int b, int c)
+        {
+        bool r = a == b && b == c;
+        return r;
+        }
+    }
+}
